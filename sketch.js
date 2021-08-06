c3a35bdcf9001fc1a97b74f16ed56979e4b67900
BananaAppleCoder <mohapatra9@gmail.com>
2021-08-05 21:06:09 -0500

    finish

--- sketch.js
+++ sketch.js
@@ -1,29 +1,42 @@
-var ball;
+var Hball, Database, position
 
 function setup(){
+    Database = firebase.database()
     createCanvas(500,500);
-    ball = createSprite(250,250,10,10);
-    ball.shapeColor = "red";
+    Hball = createSprite(250,250,10,10);
+    Hball.shapeColor = "red";
+
+    var HballPos = Database.ref("ball/position")
+    HballPos.on("value", readPosition)
 }
 
 function draw(){
     background("white");
     if(keyDown(LEFT_ARROW)){
-        changePosition(-1,0);
+        writePosition(-1,0);
     }
     else if(keyDown(RIGHT_ARROW)){
-        changePosition(1,0);
+        writePosition(1,0);
     }
     else if(keyDown(UP_ARROW)){
-        changePosition(0,-1);
+        writePosition(0,-1);
     }
     else if(keyDown(DOWN_ARROW)){
-        changePosition(0,+1);
+        writePosition(0,1);
     }
     drawSprites();
 }
 
-function changePosition(x,y){
-    ball.x = ball.x + x;
-    ball.y = ball.y + y;
+function writePosition(x,y){
+    Database.ref('ball/position').set({
+        'x':position.x + x,
+        'y': position.y + y
+    })  
+}
+
+function readPosition(data){
+    position = data.val()
+    console.log(position.x)
+    Hball.x = position.x;
+    Hball.y = position.y;
 }
